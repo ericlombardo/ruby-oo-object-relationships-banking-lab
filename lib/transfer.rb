656c3b5181725aed719ff6c942f9db5489cef928
Eric <eric.m.lombardo@gmail.com>
2020-12-05 22:10:06 -0500

    created classes to execute a transfer between two instance of the same BankAccount class

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)  # takes in transfer details
+    @sender = sender      # sets instance variables
+    @receiver = receiver  # for the transfer
+    @amount = amount      
+    @status = "pending"   # sets status to pending to start
+  end
+
+  def valid?  # calls BankAccount instances to check validity
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction # validates transfer criteria 
+    if transfer_qualifies
+      transaction_process(sender, receiver)
+      @status = "complete"      # shows transfer is complete
+    else
+      @status = "rejected"  # shows as rejected
+      "Transaction rejected. Please check your account balance."  # rejection message
+    end
+  end
+
+  def reverse_transfer 
+    if @status == "complete"  # transfer must be complete to happen
+      transaction_process(receiver, sender)
+      @status = "reversed"  # updates status to show transfer was reversed
+    end
+  end
+
+  def transfer_qualifies # makes sure transfer can be executed
+    self.valid? && sender.balance - amount > 0 && @status == "pending"
+  end
+
+  def transaction_process(sender, receiver)
+    sender.balance -= amount    # makes withdraw from sender
+    receiver.deposit(amount)  # makes deposit in receiver
+  end
 end
